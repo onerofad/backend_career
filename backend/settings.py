4eb3f848cb8242d9755732b35ae418c6917f59db
frank@gmail.com <imafidonfrank2015@gmail.com>
2023-11-19 20:52:20 +0100

    first commit

--- backend/settings.py
+++ backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+-+6x$a8_$^@-r=tz96+3ycbal@^-0#*4xn@5koiexokr48qgv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1', 'localhost']
 
 
 # Application definition
